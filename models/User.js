61f3efd614f756aba3fe4aa00869387d8ea96f13
franksabogal04 <franksabogal04@gmail.com>
2023-03-28 18:36:50 -0400

    sending the user and index

--- models/User.js
+++ models/User.js
@@ -56,3 +56,43 @@
 // );
 
 // module.exports = User;
+/////////////////////////////////////////
+
+
+const bcrypt = require("bcryptjs");
+
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.addHook("beforeCreate", user => {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+  return User;
+};
