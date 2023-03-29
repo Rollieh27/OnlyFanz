e729ccda4ecae9d8acc4d4b37097e233509ee40c
Cmiller1517 <119270268+Cmiller1517@users.noreply.github.com>
2023-03-28 20:59:02 -0400

    added in descriptions to products

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Product extends Model {}
+class product extends Model {}
 
-Product.init(
+product.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
       type: DataTypes.STRING,
       allowNull: false,
     },
