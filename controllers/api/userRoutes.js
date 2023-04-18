a380d0821035e6ce89c861152a3b3adfe4d29f62
Rollieh27 <119282670+Rollieh27@users.noreply.github.com>
2023-04-17 23:40:08 -0400

    Revert "Revert "Merge pull request #11 from Rollieh27/chadsz-cool-branch""
    This reverts commit 27f64f7bb2de2fa407b56ad5966d34bcd17ed62c.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,6 +17,7 @@ const { User } = require('../../models');
 // });
 
 router.post('/login', async (req, res) => {
+  console.log("HIT LOGIN ROUTE")
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
